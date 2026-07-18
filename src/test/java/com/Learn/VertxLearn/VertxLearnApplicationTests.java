9646c77cc42168704665dd27023de13445c2a6dd
Harshalszz <477harshz@gmail.com>
2026-07-18 20:44:10 +0530

    vertx learn

---
+++ src/test/java/com/Learn/VertxLearn/VertxLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Learn.VertxLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertxLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
